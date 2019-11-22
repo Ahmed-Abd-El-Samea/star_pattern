87be4f2ef8e2098c48a52f5e797a7792646c4a23
Ahmed <ahmedabdo6868@gmail.com>
2019-11-22 14:14:43 +0200

    Add variable tiling count

--- js/polygon.js
+++ js/polygon.js
@@ -1,4 +1,5 @@
 function Polygon(){
+    // this.sides;
     this.vertices = [];
     this.edges = [];
 
--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,7 @@
 var polys = [];
 var angle = 60;
 var delta = 20;;
+var tiles = 1;
 
 var deltaSlider;
 var angleSlider;
@@ -11,9 +12,10 @@ function setup(){
     background(51);
     deltaSlider = createSlider(0,25,0);
     angleSlider = createSlider(0,90,45);
+    tilesSlider = createSlider(1,20,1);
     angleMode(DEGREES);
 
-    var inc = 50;
+    var inc = width/tiles;
     for(var x = 0; x < width; x+= inc){
         for(var y = 0; y < height; y+= inc){
             var poly = new Polygon();
@@ -32,6 +34,22 @@ function draw(){
     background(51);
     angle = angleSlider.value();
     delta = deltaSlider.value();
+    tiles = tilesSlider.value();
+
+    polys = [];
+    var inc = width/tiles;
+    for(var x = 0; x < width; x+= inc){
+        for(var y = 0; y < height; y+= inc){
+            var poly = new Polygon();
+            poly.addVertex(x,y);
+            poly.addVertex(x+inc,y);
+            poly.addVertex(x+inc,y+inc);
+            poly.addVertex(x,y+inc);
+            poly.close();
+            polys.push(poly);
+        }
+    }
+
     for(var i = 0; i < polys.length; i++){
         polys[i].hankin();
         polys[i].show();
